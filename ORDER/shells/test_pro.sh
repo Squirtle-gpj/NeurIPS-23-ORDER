5064a1de032488034c4f37d822789ac4465c3969
Pengjie Gu <18428377446@163.com>
2024-07-15 16:08:12 +0800

    some modifications

--- ORDER/shells/test_pro.sh
+++ ORDER/shells/test_pro.sh
@@ -7,10 +7,12 @@ set -x
 
 
 script_name="main.py"
+script_name2="run_vae_policy.py"
 experiment_name="debug_distributional"
 code_name="debug_main"
 env_name="hopper-medium-v2"
-seed=1234
+seeds=(1,2,3,4,5)
+
 
 
 
@@ -19,22 +21,41 @@ timestamp=$(date +%Y%m%d%H%M%S)
 
 
 
-
-# 训练命令
-training_commands=$(cat <<EOF
-python3 ../${script_name} \
-        --experiment_name ${experiment_name} \
-        --code_name ${code_name} \
-        --env_name ${env_name} \
-        --seed ${seed}
+for seed in "${seeds[@]}"; do
+  # 训练命令
+  training_commands=$(cat <<EOF
+  python3 ../${script_name} \
+          --experiment_name ${experiment_name} \
+          --code_name ${code_name} \
+          --env_name ${env_name} \
+          --seed ${seed}
 EOF
 )
-    # 打印训练命令
-  echo "$training_commands"
-  # 执行训练命令
-  if [[ ${1} != "slurm" ]]; then
-      $training_commands
-  fi
+      # 打印训练命令
+    echo "$training_commands"
+    # 执行训练命令
+    if [[ ${1} != "slurm" ]]; then
+        $training_commands
+    fi
+    critic_path="../../Experiments/debug_distributional/Checkpoints/debug_main_${seed}/best.pt"
+
+    training_commands=$(cat <<EOF
+  python3 ../${script_name2} \
+          --experiment_name ${experiment_name} \
+          --code_name ${code_name} \
+          --env_name ${env_name} \
+          --seed ${seed} \
+          --critic_path ${critic_path}
+EOF
+)
+      # 打印训练命令
+    echo "$training_commands"
+    # 执行训练命令
+    if [[ ${1} != "slurm" ]]; then
+        $training_commands
+    fi
+
+done
 
 
 
